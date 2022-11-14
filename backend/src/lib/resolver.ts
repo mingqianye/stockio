cf0a1778371155a28058b5f728a7cf10fe2a1747
Mingqian Ye <mingqian.ye@gmail.com>
2022-11-13 23:30:28 -0800

    refactored resolver.ts

--- backend/src/lib/resolver.ts
+++ backend/src/lib/resolver.ts
@@ -1,95 +1,137 @@
 import { v4 as uuid } from "uuid";
 import { map, mergeMap, Observable } from "rxjs";
 import { RoomId, UserId } from "../shared/protocols/model";
-import { MsgClientToServer } from "../shared/protocols/MsgClientToServer";
+import { EnterRandomRoomReq, MsgClientToServer, Req } from "../shared/protocols/MsgClientToServer";
 import { OutGoingMsg } from "./connection";
 import { match, P } from "ts-pattern";
+import { produce } from "immer"
+import { flow } from "fp-ts/lib/function";
+import { Option } from "fp-ts/lib/Option";
+import { option } from "fp-ts";
 
-class State {
-  _roomMap: Record<RoomId, Room> = {}
+/////////////////////////// Types
 
-  createRoom(uid: UserId): Room {
-    const room = {
-      id: RoomId(uuid().slice(0, 5)),
-      userIds: new Set<UserId>().add(uid)
-    }
-    this._roomMap[room.id] = room
-    return room
-  }
+type Flow = (req: MsgClientToServer) => OutGoingMsg | OutGoingMsg[]
 
-  enterRoom(uid: UserId, roomId: RoomId): Room {
-    this._roomMap[roomId].userIds.add(uid)
-    return this._roomMap[roomId]
-  }
-
-  enterRandomRoom(uid: UserId): Room {
-    const room = Object.values(this._roomMap)[0]
-    room.userIds.add(uid)
-    return room
-  }
-
-  leaveRoom(uid: UserId, roomId: RoomId): Room {
-    const room = this._roomMap[roomId]
-    room.userIds.delete(uid)
-    return room
-  }
-
-}
-
-const state = new State()
-
-type Room = {
-  id: RoomId
-  userIds: Set<UserId>
-}
-
-function roomDetailRes(room: Room): OutGoingMsg {
-  return {
-    userIds: [...room.userIds],
-    msg: {
-      kind: "RoomDetailRes",
-      roomId: room.id,
-      userIds: [...room.userIds],
-      roomIsReady: false,
-      ts: new Date()
-    }
-  }
-}
-
-export const resolve = (reqObservable: Observable<MsgClientToServer>) =>
-  reqObservable.pipe(
-    map(translate),
-    mergeMap(x => toArray<OutGoingMsg>(x)),
+namespace Ping {
+  export const theFlow: Flow = flow(
+    extractUid,
+    pongRes
   )
 
-const translate = (req: MsgClientToServer) =>
-  match(req)
-    .with({ kind: "PingReq" },
-      () => pongRes(req.userId))
-    .with({ kind: "EnterRandomRoomReq" },
-      () => roomDetailRes(state.enterRandomRoom(req.userId)))
-    .with({ kind: "LeaveRoomReq", room_id: P.select() }, 
-      (roomId) => roomDetailRes(state.leaveRoom(req.userId, roomId)))
-    .with({ kind: "OrderReq" }, 
-      () => serverErrorRes(req.userId, "Not implemented"))
-    .exhaustive()
+  function pongRes(uid: UserId): OutGoingMsg {
+    return {
+      userIds: [uid],
+      msg: {
+        kind: "PongRes",
+        ts: new Date()
+      }
+    }
+  }
+}
 
-const pongRes = (uid: UserId): OutGoingMsg => ({
-  userIds: [uid],
-  msg: {
-    kind: "PongRes",
-    ts: new Date()
-  }})
+//////////////////////// Flows
+namespace Room {
+  type State = {
+    roomMap: Record<RoomId, Room>
+  }
 
-const serverErrorRes = (uid: UserId, err: string): OutGoingMsg => ({
+  type Room = {
+    id: RoomId
+    userIds: Set<UserId>
+  }
+
+  const state: State = {
+    roomMap: {}
+  }
+
+  export const EnterRandomRoomFlow: Flow = flow(
+    extractUid,
+    createRoom,
+    upsertRoom,
+    roomDetailRes
+  )
+
+  export const LeaveRoomFlow: Flow = flow(
+    extractUid,
+    removeUser,
+    (rooms) => rooms.map(roomDetailRes)
+  )
+
+  function createRoom(userId: UserId): Room {
+    return {
+      id: RoomId(uuid().slice(0, 5)),
+      userIds: new Set<UserId>().add(userId)
+    }
+  }
+
+  function removeUser(userId: UserId) {
+    return Object.values(state.roomMap)
+      .map(room => {
+        room.userIds.delete(userId)
+        return room
+      })
+      .filter(room => room.userIds.size == 0)
+      .map(deleteRoom)
+  }
+
+  function deleteRoom(r: Room): Room {
+    delete state.roomMap[r.id]
+    return r
+  }
+
+  function upsertRoom(r: Room): Room {
+    state.roomMap[r.id] = r
+    return r
+  }
+
+  function roomDetailRes(room: Room): OutGoingMsg {
+    return {
+      userIds: [...room.userIds],
+      msg: {
+        kind: "RoomDetailRes",
+        roomId: room.id,
+        userIds: [...room.userIds],
+        roomIsReady: false,
+        ts: new Date()
+      }
+    }
+  }
+}
+
+function extractUid(req: MsgClientToServer): UserId {
+  return req.userId
+}
+
+function serverErrorRes(uid: UserId, err: string): OutGoingMsg {
+  return {
     userIds: [uid],
     msg: {
       kind: "ServerErrorRes",
       error: err,
       ts: new Date()
-    }})
+    }}
+}
 
 function toArray<T>(t: T | T[]): T[] {
   const empty: T[] = []
   return empty.concat(t)
 }
+
+
+//////////////////////////// Entrypoint
+const flowMap = new Map<MsgClientToServer["kind"], Flow>()
+  .set("PingReq", Ping.theFlow)
+  .set("EnterRandomRoomReq", Room.EnterRandomRoomFlow)
+  .set("LeaveRoomReq", Room.LeaveRoomFlow)
+
+const errorFlow = (err: string) => (req: MsgClientToServer) => serverErrorRes(req.userId, err)
+
+const translate: Flow = (msg: MsgClientToServer) => 
+  (flowMap.get(msg.kind) || errorFlow(`Handler not Implemented for ${msg.kind}`))(msg)
+
+export const resolve = (reqObservable: Observable<MsgClientToServer>) =>
+  reqObservable.pipe(
+    map(translate),
+    mergeMap(toArray)
+  )
