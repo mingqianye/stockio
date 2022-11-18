10709251f934bfa188e92cb7749eb593a56be841
userpan <userpan610@gmail.com>
2022-11-18 21:27:30 +0800

    update: 获取用户code

--- clients/wechat/miniprogram/pages/index/index.ts
+++ clients/wechat/miniprogram/pages/index/index.ts
@@ -4,6 +4,8 @@ import { create } from "../../client/client"
 import { RoomId, UserId } from "../../client/shared/protocols/model"
 import { PongRes, RoomDetailRes, TickRes } from '../../client/shared/protocols/MsgServerToClient';
 import IAppOption from "../../interface/IAppOption";
+import { promisify } from "../../utils/util"
+
 const app = getApp<IAppOption>()
 
 Page({
@@ -33,37 +35,18 @@ Page({
     // await app.globalData.socket.sendReq({kind: "PingReq"})
     
     // @ts-ignore
-    if (wx.getUserProfile) {
-      this.setData({
-        canIUseGetUserProfile: true
-      })
-    }
   },
   async onShow() {
-    
     // await app.globalData.socket.sendReq({kind: "PingReq"})
-    console.log(app.globalData.socket)
+    // console.log(app.globalData.socket)
+    await this.getUserInfo()
   },
 
-  getUserProfile() {
-    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
-    // wx.getUserProfile({
-    //   desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-    //   success: (res) => {
-    //     console.log(res)
-    //     this.setData({
-    //       userInfo: res.userInfo,
-    //       hasUserInfo: true
-    //     })
-    //   }
-    // })
-  },
-  getUserInfo(e: any) {
-    // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
-    console.log(e)
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+  wxLogin: promisify(wx.login),
+  
+  async getUserInfo() {
+    const wxloginRes: any = await this.wxLogin();
+    app.globalData.userInfo = wxloginRes.code
+    console.log(app.globalData.userInfo)
   }
 })
--- clients/wechat/miniprogram/utils/util.ts
+++ clients/wechat/miniprogram/utils/util.ts
@@ -17,3 +17,20 @@ const formatNumber = (n: number) => {
   const s = n.toString()
   return s[1] ? s : '0' + s
 }
+
+//将wx的函数Promise化
+export const promisify = (fn: any) => {
+  return function (obj: any = {}) {
+    return new Promise((resolve, reject) => {
+      obj.success = function (res: any) {
+        resolve(res)
+      }
+
+      obj.fail = function (res: any) {
+        reject(res)
+      }
+
+      fn(obj)//执行函数，obj为传入函数的参数
+    })
+  }
+}
