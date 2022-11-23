99a563a8152b3fb22a56325985cb59d10485e88f
userpan <userpan610@gmail.com>
2022-11-23 16:04:26 +0800

    update: 首页优化

--- clients/wechat/miniprogram/pages/index/index.ts
+++ clients/wechat/miniprogram/pages/index/index.ts
@@ -8,43 +8,45 @@ import { promisify } from "../../utils/util"
 import { StockioClient } from "../../client/shared/clientCore";
 
 const app = getApp<IAppOption>()
+var stockioClient: StockioClient | undefined
 
 Page({
-  data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    canIUseGetUserProfile: false,
-    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
-  },
-  // 事件处理函数
-  goTeam() {
-    let stockioClient = app.globalData.stockioClient
-    console.log(stockioClient)
-    stockioClient.onPongRes((res: PongRes) => console.log("--->", JSON.stringify(res)))
-    stockioClient.sendReq({kind: "PingReq"})
-    wx.navigateTo({
-      url: '../team/team',
-    })
-  },
+  data: {},
+
+  // 生命周期
   async onLoad() {
   },
   async onShow() {
     await this.getUserInfo()
     await this.createStockioClient()
+    stockioClient = app.globalData.stockioClient
   },
 
+  // 事件处理函数
+  onTeamClicked() {
+    // let stockioClient = app.globalData.stockioClient
+    // stockioClient?.onPongRes((res: PongRes) => console.log("--->", JSON.stringify(res)))
+    // stockioClient?.sendReq({kind: "PingReq"})
+    wx.navigateTo({
+      url: '../team/team',
+    })
+  },
+  onRoomClicked() {
+
+  },
+  
+  // 创建stockioClient，在此初始化
   async createStockioClient() {
     const stockioClient: StockioClient = await create({ userId: UserId(app.globalData.userInfo) })
     app.globalData.stockioClient = stockioClient
   },
 
+  // 对login进行promise化
   wxLogin: promisify(wx.login),
 
+  // 获取用户信息
   async getUserInfo() {
     const wxloginRes: any = await this.wxLogin();
     app.globalData.userInfo = wxloginRes.code
-    console.log(app.globalData.userInfo)
   }
 })
